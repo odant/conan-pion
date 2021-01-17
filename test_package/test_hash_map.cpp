20a675c9e40dfe5e0f0725da0bea1a40dba359c5
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-01-17 04:41:31 +0300

    test c++17 standard

---
+++ test_package/test_hash_map.cpp
@@ -0,0 +1,13 @@
+/*
+ * Test C++17 compatibility
+ * Dmitriy Vetutnev, Odant 2021
+*/
+
+
+#include <pion/hash_map.hpp>
+#include <cstdlib>
+
+int main() {
+    pion::ihash_multimap i;
+    return EXIT_SUCCESS;
+}
